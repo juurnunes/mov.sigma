df889f3c957d46c084000a5843e2db1c116e45db
José Rodrigues <107969992+juurnunes@users.noreply.github.com>
2023-01-11 14:10:28 -0300

    Database
    Migrações de banco de dados para o MSSQL e alguns ajustes de compatibilidade.

--- mov/settings.py
+++ mov/settings.py
@@ -77,11 +77,26 @@ WSGI_APPLICATION = 'mov.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'mssql',
+        'NAME': 'teste2',
+        'USER': 'sa',
+        'PASSWORD': 'Mbm001@',
+        'HOST': '54.232.113.39',
+        'PORT': '1433',
+
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server',
+        },
+    },
 }
 
 
